9676dbce7b25bd4529e276ac2a40c645a8b0ba9d
FrancisUchegbu <uchegbuudemezue@gmail.com>
2021-07-18 16:12:48 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,7 +5,8 @@ class minecraft {
   }
   file {'/opt/minecraft/server.jar':
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar'
+    source => 'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar',
+    before => Service['minecraft'],
   
   }
   package {'java':
@@ -24,6 +25,7 @@ class minecraft {
   service {'minecraft':
     ensure => running,
     enable => true,
+    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']],
   
   }
 
