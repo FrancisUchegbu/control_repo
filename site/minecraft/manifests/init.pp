65ce0b6be82f07c3d5baef8c8864d9eb2449b2ba
FrancisUchegbu <uchegbuudemezue@gmail.com>
2021-07-18 16:55:47 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,5 +1,5 @@
 class minecraft {
-$url = 'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar',
+$url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
 $install_dir = '/opt/minecraft',
   file{$install_dir:
     ensure => directory,
